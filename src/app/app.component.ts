f5335ede697f8669cf70567f946cae3fa33aed9a
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-02 13:22:07 +0530

    ANGULAR INTERPOLATION STUDY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KULDEEP';
+  name = 'kuldeep dhasmana'
+  
+getName()
+{
+  return this.name
+}
+obj={ 
+  name :'jackal',
+   age : 200
+}
+arr=['peter' , 'goAir', 'VISTARA']
+a=200;
+b=50;
+siteUrl = window.location.href
 }
