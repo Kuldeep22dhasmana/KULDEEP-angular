73b33ddd0fb1f49c85909af600916be671d64bf1
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-03 13:27:28 +0530

    get text box value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KULDEEP';
-  getName(){
-    alert("cooldeep")
+  getVal(event:any)
+  {
+    var val = event.target.value 
+    console.log(val)
   }
 }
