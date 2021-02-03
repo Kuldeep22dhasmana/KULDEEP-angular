018403584a38ed1138fae8bcc6e8c4f65bb27c06
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-03 15:56:50 +0530

    text value with button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KULDEEP';
+  currentval = ""
   getVal(event:any)
   {
     var val = event.target.value 
     console.log(val)
+    this.currentval=val
   }
 }
