dcdd1f649ea39a16dc64ff7867bcec54bff95c87
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-04 16:17:28 +0530

    FOR LOOP IN ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KULDEEP';
-  color= "blue"
+  data=
+   [
+     {
+       name:'goli',
+       age: 43,
+       email:'rty@gmail.com'
+     },
+     {
+      name:'suchi',
+      age: 22,
+      email:'qwe@gmail.com'
+    },
+    {
+      name:'kaju',
+      age: 16,
+      email:'gbn@gmail.com'
+    },
+    {
+      name:'bheem',
+      age: 50,
+      email:'cxz@gmail.com'
+    }
+  ]
 }
