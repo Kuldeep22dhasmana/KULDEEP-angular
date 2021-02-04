789ce95cab118264eb7edd90f66c8805f344b501
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-04 18:01:58 +0530

    SIMPLE FORM IN ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KULDEEP';
-  data=
-   [
-     {
-       name:'goli',
-       age: 43,
-       email:'rty@gmail.com'
-     },
-     {
-      name:'suchi',
-      age: 22,
-      email:'qwe@gmail.com'
-    },
-    {
-      name:'kaju',
-      age: 16,
-      email:'gbn@gmail.com'
-    },
-    {
-      name:'bheem',
-      age: 50,
-      email:'cxz@gmail.com'
-    }
-  ]
+  getVal(val:any)
+  {
+    console.log(val)
+  }
 }
