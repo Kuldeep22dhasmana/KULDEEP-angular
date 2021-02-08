b6c28fb26082d969cab556acfa559651eb1e3ddd
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-08 19:16:05 +0530

    Custom rating template provided as child element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .star {
+      font-size: 1.5rem;
+      color: #b0c4de;
+    }
+    .filled {
+      color: #1e90ff;
+    }
+    .bad {
+      color: #deb0b0;
+    }
+    .filled.bad {
+      color: #ff1e1e;
+    }
+  `]
 })
 export class AppComponent {
   title = 'KULDEEP';
-  selected = 0;
-  hovered = 0;
-  readonly = false;
+  currentRate = 5;
 }
 
 
