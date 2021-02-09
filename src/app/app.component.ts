823fcd792f3e50084431d5c94d5d52ea603038ba
kuldeep dhasmana <dhasmanakuldeep22@gmail.com>
2021-02-09 12:39:56 +0530

    Contextual text progress bars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  styles: [`
-    .star {
-      font-size: 1.5rem;
-      color: #b0c4de;
-    }
-    .filled {
-      color: #1e90ff;
-    }
-    .bad {
-      color: #deb0b0;
-    }
-    .filled.bad {
-      color: #ff1e1e;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'KULDEEP';
-  currentRate = 5;
 }
 
 
